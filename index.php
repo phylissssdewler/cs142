6842efee19c53b5fc3738c2405ab0fbe516f5ec8
bmckalla <bmckalla@uvm.edu>
2016-03-25 00:14:24 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="Bmckalla" content="Yes">
+        <title>Description of site</title>
+        <meta name="description" content="Defying the establishment"/>
+    </head>
+    <body>
+        <header>
+            <h1 style="text-align: center; font-size: 30pt">
+                Defying the establishment
+            </h1>
+        </header>
+    <body>
+        <p>For our project, we chose to incorporate elements of a movement know as</p>
+        <h2 style="text-align: center; font-size: 15pt; color: pink; 
+            font-family: 'Broadway'; text-shadow: 1pt black;">VAPORWAVE</h2>
+        <p>It is micro-genre of music in which artists sample 80s and 90s music by
+            slowing it down "narcotically." The genre takes an approach similar
+            to that of lounge-y, jazz elevator music, but with a very twisted remix. 
+            A lot of the albums covers were 
+            defined by cool, blue, pink, and purple color schemes with sort 
+            of a late 80s and 90s feel. Occasionally using some neon related tones.
+            Think Miami Vice meets Windows 98.</p>
+        <p>Interestingly enough, we modeled this in the background with sort of
+        a quadruple hue. We also chose Broadway as a font and pink for many of 
+        text styles. The Admin menu pops out on the right side. Sort
+        of homage to "Defying the establishment", a motto of Vaporwave, which
+        often takes a satirical approach to itself and also consumer capitalism,
+        and pop-culture. Otherwise, texts appear black, and we include white, see
+        through boxes to contain most of the info.</p>
+        <p>Yes, the cursor is George Constanza.</p>
+        <p>Contributors: Alex Waldron, Braden McKallagat, Sam Tully</p>
+    </body>
+</html>
